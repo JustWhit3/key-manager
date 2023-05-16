cce097bd21828e814d115ac4c6101cebd2e31879
Carl Timmer <timmer@jlab.org>
2023-05-16 20:02:34 +0000

    Update docs

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -3,6 +3,6 @@ var searchData=
   ['encrypt_41',['encrypt',['../classkmanager_1_1utility_1_1Crypto.html#ab6c012a2ed86782d071dbd7c9e374940',1,'kmanager::utility::Crypto']]],
   ['enter_5fpassword_42',['enter_password',['../classkmanager_1_1state_1_1LoginState.html#a6d16e72bad2e82f022ae99c856c7b39e',1,'kmanager::state::LoginState']]],
   ['enter_5fpassword_5flabel_43',['enter_password_label',['../classkmanager_1_1state_1_1LoginState.html#a16c6cdc04d12c5e04512200db6e82526',1,'kmanager::state::LoginState']]],
-  ['error_5flabel_44',['error_label',['../classkmanager_1_1state_1_1AddPasswordState.html#ad25bcbccc238870c010ac4390d8cedc3',1,'kmanager::state::AddPasswordState']]],
+  ['error_5flabel_44',['error_label',['../classkmanager_1_1state_1_1AddPasswordState.html#ad25bcbccc238870c010ac4390d8cedc3',1,'kmanager::state::AddPasswordState::error_label()'],['../classkmanager_1_1state_1_1LoginState.html#a5dfe34e8462d9e5452b07c75343c09b1',1,'kmanager::state::LoginState::error_label()']]],
   ['exit_5fbutton_45',['exit_button',['../classkmanager_1_1state_1_1MenuState.html#a8755a587f4f17f50ce0693ce69d156cb',1,'kmanager::state::MenuState']]]
 ];
--- docs/search/variables_3.js
+++ docs/search/variables_3.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['enter_5fpassword_260',['enter_password',['../classkmanager_1_1state_1_1LoginState.html#a6d16e72bad2e82f022ae99c856c7b39e',1,'kmanager::state::LoginState']]],
   ['enter_5fpassword_5flabel_261',['enter_password_label',['../classkmanager_1_1state_1_1LoginState.html#a16c6cdc04d12c5e04512200db6e82526',1,'kmanager::state::LoginState']]],
-  ['error_5flabel_262',['error_label',['../classkmanager_1_1state_1_1AddPasswordState.html#ad25bcbccc238870c010ac4390d8cedc3',1,'kmanager::state::AddPasswordState']]],
+  ['error_5flabel_262',['error_label',['../classkmanager_1_1state_1_1AddPasswordState.html#ad25bcbccc238870c010ac4390d8cedc3',1,'kmanager::state::AddPasswordState::error_label()'],['../classkmanager_1_1state_1_1LoginState.html#a5dfe34e8462d9e5452b07c75343c09b1',1,'kmanager::state::LoginState::error_label()']]],
   ['exit_5fbutton_263',['exit_button',['../classkmanager_1_1state_1_1MenuState.html#a8755a587f4f17f50ce0693ce69d156cb',1,'kmanager::state::MenuState']]]
 ];
