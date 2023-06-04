452859992dc7691411a0546d497c0abb818a8317
Carl Timmer <timmer@jlab.org>
2023-06-04 19:05:25 +0000

    Update docs

--- docs/search/all_f.js
+++ docs/search/all_f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['rectangle_142',['rectangle',['../classkmanager_1_1state_1_1PasswordGeneratorState.html#a5a9090c1fc1429066a6bc44e44edd17f',1,'kmanager::state::PasswordGeneratorState']]]
+  ['rectangle_142',['rectangle',['../classkmanager_1_1state_1_1LoginState.html#a63d18283d03e3d7a95803ade1c06923c',1,'kmanager::state::LoginState::rectangle()'],['../classkmanager_1_1state_1_1PasswordGeneratorState.html#a5a9090c1fc1429066a6bc44e44edd17f',1,'kmanager::state::PasswordGeneratorState::rectangle()']]]
 ];
